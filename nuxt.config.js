8a90cb772b94de99df355e502f638052432a5ac6
Diego <d.delajaral@gmail.com>
2018-11-18 09:48:28 -0300

    primer commit

---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Covit',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js + Vuetify.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons' },
+    ]
+  },
+  plugins: [
+    '~/plugins/vuetify.js'
+  ],
+  css: [
+    '~/assets/style/style.css'
+  ],
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { 
+    color: '#3B8070' 
+  },
+  
+  manifest: {
+    theme_color: '#00aeef'
+  },
+  /*
+  ** Build configuration
+  */
+  modules: [
+      ['@nuxtjs/pwa', { icon: true }]
+  ]
+}
---
+++ pages/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app>
+    <core-filter />
+
+    <core-toolbar />
+
+    <core-drawer />
+
+  </v-app>
+</template>
+
+<script>
+	import CoreFilter  from '../components/core/Filter'
+	import CoreToolbar  from '../components/core/Toolbar'
+	import CoreDrawer  from '../components/core/Drawer.vue'
+
+	export default {
+		components: {
+			CoreFilter,
+			CoreToolbar,
+			CoreDrawer
+		}
+	}
+</script>
+
+<!--style lang="scss">
+@import '@/styles/index.scss';
+
+/* Remove in 1.2 */
+.v-datatable thead th.column.sortable i {
+  vertical-align: unset;
+}
+</style-->
